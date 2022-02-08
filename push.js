326c1478635fcc3318329aba16c5a2d563c9fae4
deepak3dat <deepak3dart@gmail.com>
2022-02-08 12:26:50 +0530

    initial

---
+++ push.js
@@ -0,0 +1,6 @@
+var push =require('web-push');
+
+let vapidkeys={
+    publicKey: 'BPArWEZiWr3R-QwLQs2KR8VY6UUmjlmgBwIh395HTSlLKEpgS-GRo_nlqiSLL72biNDumTFHlzWY3mldPYUVEJQ',
+    privateKey: 'WruLAjunwite52SKUajbOMXRo-M4ktbl3KlSFvVIgCk'
+  }
