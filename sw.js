f7451f9fa017360e06a94da7595bfa09f6b297fc
deepak3dart <deepak3dart@gmail.com>
2022-02-08 14:02:05 +0530

    update 2

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('push',()=>{
-    self.ServiceWorkerRegistration.sendNotification('test message 2.',{});
-});
+self.addEventListener('push', function(e) {
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/android-launchericon-48-48.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'explore', title: 'Explore this new world',
+          icon: 'images/outline_check_circle_black_24dp.png'},
+        {action: 'close', title: 'Close',
+          icon: 'images/icons8-blogger-60.png'},
+      ]
+    };
+    e.waitUntil(
+      self.registration.showNotification('Hello world!', options)
+    );
+  });
